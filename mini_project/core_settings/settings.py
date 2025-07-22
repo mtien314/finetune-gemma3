d96e4030f50b725f6a11a1c5fddc716639cc600c
mtien314 <90877954+mtien314@users.noreply.github.com>
2025-07-22 21:31:07 +0700

    Delete mini_project directory

--- mini_project/core_settings/settings.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv(override=True)
-
-class Settings(BaseSettings):
-    GOOGLE_API_KEY: str 
-
-settings = Settings()
